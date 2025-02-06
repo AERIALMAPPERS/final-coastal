d33e1f494a5ca0281c6323c9f94d3b2bfebdf787
AERIALMAPPERS <vrushaliamdatalab@gmail.com>
2025-02-06 15:33:17 +0530

    add

--- js/gallery.js
+++ js/gallery.js
@@ -35,3 +35,18 @@ document.addEventListener("DOMContentLoaded", loadImages);
     }
 
     ///////////////////////////////////////////
+
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement({
+          pageLanguage: 'en',
+          includedLanguages: 'en,hi,mr'  // Add languages you want to include
+      }, 'google_translate_element');
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+      setTimeout(function () {
+          let votingForm = document.getElementById("goog-gt-votingForm");
+          let votingPane = document.getElementById("goog-gt-votingHiddenPane");
+          if (votingForm) votingForm.remove();
+          if (votingPane) votingPane.remove();
+      }, 3000); // Delay to ensure elements are injected before removal
+    });
