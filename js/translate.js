b9e65baa60bd29404b46a6f59612ab82ab5be65b
AERIALMAPPERS <vrushaliamdatalab@gmail.com>
2025-02-06 12:58:48 +0530

    add

--- js/translate.js
+++ js/translate.js
@@ -1,34 +1,16 @@
+
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
-    pageLanguage: 'en',
-    includedLanguages: 'en,hi,mr',  // Add languages you want to include
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+      pageLanguage: 'en',
+      includedLanguages: 'en,hi,mr'  // Add languages you want to include
   }, 'google_translate_element');
 }
-
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
-    let votingForm = document.getElementById("goog-gt-votingForm");
-    let votingPane = document.getElementById("goog-gt-votingHiddenPane");
-
-    // Check if the elements exist and remove them
-    if (votingForm) {
-      votingForm.remove();
-    }
-    if (votingPane) {
-      votingPane.remove();
-    }
-
-    // Add ARIA labels if the dynamic form elements exist
-    let translateDropdown = document.querySelector('.goog-te-combo');
-    if (translateDropdown) {
-      translateDropdown.setAttribute('aria-label', 'Select your language');
-    }
-
-    let translateButton = document.querySelector('.goog-te-button');
-    if (translateButton) {
-      translateButton.setAttribute('aria-label', 'Translate page');
-    }
-
+      let votingForm = document.getElementById("goog-gt-votingForm");
+      let votingPane = document.getElementById("goog-gt-votingHiddenPane");
+      if (votingForm) votingForm.remove();
+      if (votingPane) votingPane.remove();
   }, 3000); // Delay to ensure elements are injected before removal
 });
+
